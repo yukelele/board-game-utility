5eda848e2e46b48d78afccbf3c08ae1d89b07bfd
Yuki Mano <ysm@umail.ucsb.edu>
2019-08-11 12:04:54 -0400

    responsive menu example

--- board-game-utility/src/app/app.component.ts
+++ board-game-utility/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('myTopnav') myTopnav: ElementRef;
 
+  constructor() {}
+
+  ngOnInit() {    
+  }
+
+  myFunction() {
+    let x = this.myTopnav;
+
+    if (x.nativeElement.classList.contains('responsive')) {
+      x.nativeElement.classList.remove('responsive');
+    } else {
+      x.nativeElement.classList.add('responsive');
+    }
+  }
   
 }
